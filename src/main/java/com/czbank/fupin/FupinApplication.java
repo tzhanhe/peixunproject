7d80bbcf25b9c30ffd6208ea0806131da3753b1b
fuhuozhishou <745634157@qq.com>
2019-07-11 15:52:42 +0800

    项目框架

---
+++ src/main/java/com/czbank/fupin/FupinApplication.java
@@ -0,0 +1,13 @@
+package com.czbank.fupin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FupinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FupinApplication.class, args);
+    }
+
+}
