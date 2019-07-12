eb31f0385800b3a39daa26877736d94b8ee9bbc5
fuhuozhishou <745634157@qq.com>
2019-07-12 19:13:38 +0800

    product

--- src/main/java/com/czbank/fupin/domain/Product.java
+++ src/main/java/com/czbank/fupin/domain/Product.java
@@ -1,14 +1,27 @@
 package com.czbank.fupin.domain;
 
 import java.io.Serializable;
-
+/**
+ * @Author Leo
+ * @Description //TODO
+ * @Date 2019/7/12
+ **/
 public class Product implements Serializable {
+
     private int id;
     private String productName;
     private double price;
+    //商品简介
     private String info;
+    //库存
     private int stock;
+    //所属省的id
+    private int provinceId;
+    //所属县的id
+    private int countyId;
+    //所属用户的id
     private int ownerId;
+
 
     public int getId() {
         return id;
@@ -56,6 +69,22 @@ public class Product implements Serializable {
 
     public void setOwnerId(int ownerId) {
         this.ownerId = ownerId;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getCountyId() {
+        return countyId;
+    }
+
+    public void setCountyId(int countyId) {
+        this.countyId = countyId;
     }
 
     @Override
